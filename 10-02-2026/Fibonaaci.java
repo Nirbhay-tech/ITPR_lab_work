b24817d980c8665df9756abca9a225d6879979fe
Nirbhay-tech <nirbhayruhela0001@gmail.com>
2026-02-10 11:42:25 +0530

    Update Fibonaaci.java

--- 10-02-2026/Fibonaaci.java
+++ 10-02-2026/Fibonaaci.java
@@ -1,17 +1,18 @@
 class Fibonaaci{
+    // creating a main method
     public static void main(String[] args){
+        
         int a=0;
         int b=1;
 
+    // for loop to print fibonaaci
         for(int i=1;i<=30;i++){
             System.out.println(a+" ");
         
-        int next=a+b;
+        int next=a+b; //contains next value
         a=b;
         b=next;
         }
-   
-    }
-
+   }
 }
 
